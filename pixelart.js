e251a43e478a7b70a724c87ad2579ef6cbb934fb
ciwj <zahirabakr@gmail.com>
2026-09-19 19:28:15 -0400

    canvas work

--- pixelart.js
+++ pixelart.js
@@ -7,13 +7,21 @@ function init() {
     let canvas = document.getElementById("pixelart_canvas");
     let ctx = canvas.getContext("2d");                                                      // TODO if you want. learn render good
 
-    let head_shoulders = new Image(20,20);
-    head_shoulders.src = "assets/visual/dark_desk.png";
-
+    placeImage(ctx, "/assets/visual/dark_desk.png", 65, 100, 200, 200, 0);
+    placeImage(ctx, "assets/visual/sword_no_bg.png", canvas.width / 2 - 100, canvas.height / 2 - 200, 200, 400, 180);
 
     ctx.font = "40px bouba";
+}
 
-    ctx.drawImage(head_shoulders, 50, 50);
+function placeImage(ctx, src, x, y, w, h, rot) {
+    let img = new Image();
+    img.src = src;
 
-    console.log("please");
+    img.addEventListener("load", () =>{
+        if (rot !== 0) {
+            img.style.transform = "rotate(" + toString(rot) + "deg)";
+        }
+        ctx.drawImage(img, x, y, w, h);
+
+    })
 }
